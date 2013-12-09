58a8f26d233ef69e457192fe28ba08d71dc931af
Tee Parham <tee@neighborland.com>
2013-12-09 16:07:23 -0700

    add Queue.is_unique? tests

--- test/queue_test.rb
+++ test/queue_test.rb
@@ -1,5 +1,17 @@
 require 'test_helper'
 
 class QueueTest < Test::Unit::TestCase
+  context ".is_unique?" do
+    should "be false for non-unique job" do
+      refute ResqueSolo::Queue.is_unique?(class: 'FakeJob')
+    end
 
+    should "be false for invalid job class" do
+      refute ResqueSolo::Queue.is_unique?(class: 'InvalidJob')
+    end
+
+    should "be true for unique job" do
+      assert ResqueSolo::Queue.is_unique?(class: 'FakeUniqueJob')
+    end
+  end
 end
