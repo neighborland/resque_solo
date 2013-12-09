e7be5815b8f198374aac7ad93c9b96839edd4062
Tee Parham <tee@neighborland.com>
2013-12-09 16:13:42 -0700

    add Queue.item_ttl tests

--- test/queue_test.rb
+++ test/queue_test.rb
@@ -1,17 +1,38 @@
 require 'test_helper'
 
-class QueueTest < Test::Unit::TestCase
-  context ".is_unique?" do
-    should "be false for non-unique job" do
-      refute ResqueSolo::Queue.is_unique?(class: 'FakeJob')
+module ResqueSolo
+  class QueueTest < Test::Unit::TestCase
+    context ".is_unique?" do
+      should "be false for non-unique job" do
+        refute Queue.is_unique?(class: 'FakeJob')
+      end
+  
+      should "be false for invalid job class" do
+        refute Queue.is_unique?(class: 'InvalidJob')
+      end
+  
+      should "be true for unique job" do
+        assert Queue.is_unique?(class: 'FakeUniqueJob')
+      end
     end
+    
+    context ".item_ttl" do
+      should "be -1 for non-unique job" do
+        assert_equal -1, Queue.item_ttl(class: 'FakeJob')
+      end
 
-    should "be false for invalid job class" do
-      refute ResqueSolo::Queue.is_unique?(class: 'InvalidJob')
-    end
+      should "be -1 for invalid job class" do
+        assert_equal -1, Queue.item_ttl(class: 'InvalidJob')
+      end
 
-    should "be true for unique job" do
-      assert ResqueSolo::Queue.is_unique?(class: 'FakeUniqueJob')
+      should "be -1 for unique job" do
+        assert_equal -1, Queue.item_ttl(class: 'FakeUniqueJob')
+      end
+
+      should "be job TTL" do
+        assert_equal 300, UniqueJobWithTtl.ttl
+        assert_equal UniqueJobWithTtl.ttl, Queue.item_ttl(class: 'UniqueJobWithTtl')
+      end
     end
   end
 end
