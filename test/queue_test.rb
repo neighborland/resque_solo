40a141afa39cf224f6597021195bbaca41fcfdd6
Tee Parham <tee@neighborland.com>
2013-12-09 16:17:19 -0700

    add Queue.lock_after_execution_period tests

--- test/queue_test.rb
+++ test/queue_test.rb
@@ -31,7 +31,26 @@ module ResqueSolo
 
       should "be job TTL" do
         assert_equal 300, UniqueJobWithTtl.ttl
-        assert_equal UniqueJobWithTtl.ttl, Queue.item_ttl(class: 'UniqueJobWithTtl')
+        assert_equal 300, Queue.item_ttl(class: 'UniqueJobWithTtl')
+      end
+    end
+
+    context ".lock_after_execution_period" do
+      should "be 0 for non-unique job" do
+        assert_equal 0, Queue.lock_after_execution_period(class: 'FakeJob')
+      end
+
+      should "be 0 for invalid job class" do
+        assert_equal 0, Queue.lock_after_execution_period(class: 'InvalidJob')
+      end
+
+      should "be 0 for unique job" do
+        assert_equal 0, Queue.lock_after_execution_period(class: 'FakeUniqueJob')
+      end
+
+      should "be job lock period" do
+        assert_equal 150, UniqueJobWithLock.lock_after_execution_period
+        assert_equal 150, Queue.lock_after_execution_period(class: 'UniqueJobWithLock')
       end
     end
   end
