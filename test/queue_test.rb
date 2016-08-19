fd79e3a7d2a6ed6ba82e00b30f02d05aa077b73c
Tee Parham <tee@neighborland.com>
2016-08-18 21:53:45 -0600

    Remove module wrapper from test

--- test/queue_test.rb
+++ test/queue_test.rb
@@ -1,57 +1,55 @@
 require "test_helper"
 
-module ResqueSolo
-  class QueueTest < MiniTest::Spec
-    describe ".is_unique?" do
-      it "is false for non-unique job" do
-        refute Queue.is_unique?(class: "FakeJob")
-      end
-
-      it "is false for invalid job class" do
-        refute Queue.is_unique?(class: "InvalidJob")
-      end
-
-      it "is true for unique job" do
-        assert Queue.is_unique?(class: "FakeUniqueJob")
-      end
+class QueueTest < MiniTest::Spec
+  describe ".is_unique?" do
+    it "is false for non-unique job" do
+      refute ResqueSolo::Queue.is_unique?(class: "FakeJob")
     end
 
-    describe ".item_ttl" do
-      it "is -1 for non-unique job" do
-        assert_equal(-1, Queue.item_ttl(class: "FakeJob"))
-      end
-
-      it "is -1 for invalid job class" do
-        assert_equal(-1, Queue.item_ttl(class: "InvalidJob"))
-      end
-
-      it "is -1 for unique job" do
-        assert_equal(-1, Queue.item_ttl(class: "FakeUniqueJob"))
-      end
-
-      it "is job TTL" do
-        assert_equal 300, UniqueJobWithTtl.ttl
-        assert_equal 300, Queue.item_ttl(class: "UniqueJobWithTtl")
-      end
+    it "is false for invalid job class" do
+      refute ResqueSolo::Queue.is_unique?(class: "InvalidJob")
     end
 
-    describe ".lock_after_execution_period" do
-      it "is 0 for non-unique job" do
-        assert_equal 0, Queue.lock_after_execution_period(class: "FakeJob")
-      end
+    it "is true for unique job" do
+      assert ResqueSolo::Queue.is_unique?(class: "FakeUniqueJob")
+    end
+  end
 
-      it "is 0 for invalid job class" do
-        assert_equal 0, Queue.lock_after_execution_period(class: "InvalidJob")
-      end
+  describe ".item_ttl" do
+    it "is -1 for non-unique job" do
+      assert_equal(-1, ResqueSolo::Queue.item_ttl(class: "FakeJob"))
+    end
 
-      it "is 0 for unique job" do
-        assert_equal 0, Queue.lock_after_execution_period(class: "FakeUniqueJob")
-      end
+    it "is -1 for invalid job class" do
+      assert_equal(-1, ResqueSolo::Queue.item_ttl(class: "InvalidJob"))
+    end
 
-      it "is job lock period" do
-        assert_equal 150, UniqueJobWithLock.lock_after_execution_period
-        assert_equal 150, Queue.lock_after_execution_period(class: "UniqueJobWithLock")
-      end
+    it "is -1 for unique job" do
+      assert_equal(-1, ResqueSolo::Queue.item_ttl(class: "FakeUniqueJob"))
+    end
+
+    it "is job TTL" do
+      assert_equal 300, UniqueJobWithTtl.ttl
+      assert_equal 300, ResqueSolo::Queue.item_ttl(class: "UniqueJobWithTtl")
+    end
+  end
+
+  describe ".lock_after_execution_period" do
+    it "is 0 for non-unique job" do
+      assert_equal 0, ResqueSolo::Queue.lock_after_execution_period(class: "FakeJob")
+    end
+
+    it "is 0 for invalid job class" do
+      assert_equal 0, ResqueSolo::Queue.lock_after_execution_period(class: "InvalidJob")
+    end
+
+    it "is 0 for unique job" do
+      assert_equal 0, ResqueSolo::Queue.lock_after_execution_period(class: "FakeUniqueJob")
+    end
+
+    it "is job lock period" do
+      assert_equal 150, UniqueJobWithLock.lock_after_execution_period
+      assert_equal 150, ResqueSolo::Queue.lock_after_execution_period(class: "UniqueJobWithLock")
     end
   end
 end
